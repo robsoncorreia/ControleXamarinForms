85ccd54a1669d23977cbbe5bb52b7767134c7d2a
Robson Correia <robsoncorreia01@outlook.com>
2019-01-08 03:35:48 -0200

    Adicionar arquivos de projeto.

---
+++ ControleXamarinForms/ControleXamarinForms/Controls/ProgressBarPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControleXamarinForms.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProgressBarPage : ContentPage
+    {
+        public ProgressBarPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Modificar(object sender, EventArgs e)
+        {
+            Bar1.Progress = 1;
+            Bar2.ProgressTo(1, 5000, Easing.Linear);
+            Bar3.ProgressTo(1, 5000, Easing.SpringIn);
+        }
+    }
+}
---
+++ ControleXamarinForms/ControleXamarinForms/Menu/Master.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ControleXamarinForms.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControleXamarinForms.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void GoActivityIndicatorPage(object sender, EventArgs e)
+        {
+            Detail = new ActivityIndicatorPage();
+        }
+
+        private void GoProgressBarPage(object sender, EventArgs e)
+        {
+            Detail = new ProgressBarPage();
+        }
+    }
+}

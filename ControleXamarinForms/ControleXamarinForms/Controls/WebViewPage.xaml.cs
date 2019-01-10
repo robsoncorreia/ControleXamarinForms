dd64da7ac36175f0d29b25a62ba1570dbf12166f
Robson Correia <robsoncorreia01@outlook.com>
2019-01-10 16:55:33 -0200

    Navigation Page

---
+++ ControleXamarinForms/ControleXamarinForms/Controls/WebViewPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControleXamarinForms.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WebViewPage : ContentPage
+    {
+        public WebViewPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoPagina(object sender, EventArgs e)
+        {
+            WebView.Source = EnderecoSite.Text;
+        }
+
+        private void GoProximo(object sender, EventArgs e)
+        {
+            if (WebView.CanGoForward)
+            {
+                WebView.GoForward();
+            }
+        }
+
+        private void GoVoltar(object sender, EventArgs e)
+        {
+            if (WebView.CanGoBack)
+            {
+                WebView.GoBack();
+            }
+        }
+
+        private void ActionCarregando(object sender, WebNavigatingEventArgs e)
+        {
+            LblStatus.Text = "Carregando";
+        }
+
+        private void ActionCarregado(object sender, WebNavigatedEventArgs e)
+        {
+            LblStatus.Text = "Carregado";
+        }
+    }
+}
--- ControleXamarinForms/ControleXamarinForms/Menu/Master.xaml.cs
+++ ControleXamarinForms/ControleXamarinForms/Menu/Master.xaml.cs
@@ -16,91 +16,97 @@ namespace ControleXamarinForms.Menu
 
         private void GoActivityIndicatorPage(object sender, EventArgs e)
         {
-            Detail = new ActivityIndicatorPage();
+            Detail = new NavigationPage(new ActivityIndicatorPage());
             IsPresented = false;
         }
 
         private void GoProgressBarPage(object sender, EventArgs e)
         {
-            Detail = new ProgressBarPage();
+            Detail = new NavigationPage(new ProgressBarPage());
             IsPresented = false;
         }
 
         private void GoBoxViewPage(object sender, EventArgs e)
         {
-            Detail = new BoxViewPage();
+            Detail = new NavigationPage(new BoxViewPage());
             IsPresented = false;
         }
 
         private void GoLabelPage(object sender, EventArgs e)
         {
-            Detail = new LabelPage();
+            Detail = new NavigationPage(new LabelPage());
             IsPresented = false;
         }
 
         private void GoButtonPage(object sender, EventArgs e)
         {
-            Detail = new ButtonPage();
+            Detail = new NavigationPage(new ButtonPage());
             IsPresented = false;
         }
 
         private void GoEntryEditorPage(object sender, EventArgs e)
         {
-            Detail = new EntryEditorPage();
+            Detail = new NavigationPage(new EntryEditorPage());
             IsPresented = false;
         }
 
         private void GoDatePickerPage(object sender, EventArgs e)
         {
-            Detail = new DatePickerPage();
+            Detail = new NavigationPage(new DatePickerPage());
             IsPresented = false;
         }
 
         private void GoTimePickerPage(object sender, EventArgs e)
         {
-            Detail = new TimePickerPage();
+            Detail = new NavigationPage(new TimePickerPage());
             IsPresented = false;
         }
 
         private void GoPickerPage(object sender, EventArgs e)
         {
-            Detail = new PickerPage();
+            Detail = new NavigationPage(new PickerPage());
             IsPresented = false;
         }
 
         private void GoSearchBarPage(object sender, EventArgs e)
         {
-            Detail = new SearchBarPage();
+            Detail = new NavigationPage(new SearchBarPage());
             IsPresented = false;
         }
 
         private void GoSliderStepperPage(object sender, EventArgs e)
         {
-            Detail = new SliderStepperPage();
+            Detail = new NavigationPage(new SliderStepperPage());
             IsPresented = false;
         }
 
         private void GoSwitchPage(object sender, EventArgs e)
         {
-            Detail = new SwitchPage();
+            Detail = new NavigationPage(new SwitchPage());
             IsPresented = false;
         }
 
         private void GoImagePage(object sender, EventArgs e)
         {
-            Detail = new ImagePage();
+            Detail = new NavigationPage(new ImagePage());
             IsPresented = false;
         }
 
         private void GoListViewPage(object sender, EventArgs e)
         {
-            Detail = new ListViewPage();
+            Detail = new NavigationPage(new ListViewPage());
             IsPresented = false;
         }
 
         private void GoTableViewPage(object sender, EventArgs e)
         {
-            Detail = new TableViewPage();
+            Detail = new NavigationPage(new TableViewPage());
+            IsPresented = false;
+        }
+
+        private void GoWebViewPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new WebViewPage());
             IsPresented = false;
         }
     }
